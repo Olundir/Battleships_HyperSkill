0f6b78717c44c74f286d6b645e0ec6cc145e10a7
Olundir <krzysztof.jezak.contact@gmail.com>
2022-03-08 21:53:39 +0100

    Refactored the code using OOP concepts

--- src/Main.java
+++ src/Main.java
@@ -1,151 +1,163 @@
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Write your code here
-        Scanner scanner = new Scanner(System.in);
-        String[][] board = boardInit();
-        printBoard(board);
+        Ship[] ships = {new Ship("Destroyer", 2),
+                new Ship("Cruiser", 3),
+                new Ship("Submarine", 3),
+                new Ship("Battleship", 4),
+                new Ship("Aircraft Carrier", 5)};
 
-        p1PlaceShips(scanner, board);
+        Battlefield battlefield = new Battlefield(ships);
 
+        Game.initialShipPlacement(battlefield);
+    }
+}
+
+class Game extends GameRules {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void initialShipPlacement(Battlefield battlefield) {
+        battlefield.createBattlefield();
+        battlefield.printBattlefield();
+        while (battlefield.getshipNumber() != 0) {
+            System.out.printf("\nEnter the coordinates of the %s (%d cells):\n\n",
+                    battlefield.getShip().getShipName(), battlefield.getShip().getShipLength());
+                while(true) {
+                    try {
+                        battlefield.getShip().setShipCords(scanner.nextLine());
+
+                        checkGameRules(battlefield);
+
+                        battlefield.placeShip();
+                        battlefield.printBattlefield();
+                        break;
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage().contains("Error") ? "\n" + e.getMessage() :
+                                new Exception(String.format("Error! %s. Try again: " + "\n",
+                                        e.getLocalizedMessage())).getMessage());
+                    }
+                }
+        }
         scanner.close();
     }
+}
 
-    private static void printBoard(String[][] board) {
-        for (int i = 0; i < 11; i++) {
-            for (int j = 0; j < 11; j++) {
-                System.out.print(board[i][j]);
-                if (j != 10) System.out.print(" ");
+class GameRules {
+    public static void checkGameRules(Battlefield battlefield) throws Exception {
+        int[] cords = battlefield.getShip().getShipCords();
+        int length = battlefield.getShip().getShipLength();
+        int lengthFromCords = 0;
+        if (cords[0] == cords[2]) lengthFromCords = Math.abs(cords[1] - cords[3]) + 1;
+        if (cords[1] == cords[3]) lengthFromCords = Math.abs(cords[0] - cords[2]) + 1;
+        if (cords[1] > 10 || cords[3] > 10) {
+            throw new Exception("Error! Wrong ship location! Try again:\n");
+        } else if (!(cords[0] == cords[2] && cords[1] != cords[3]
+        || cords[0] != cords[2] && cords[1] == cords[3])) {
+            throw new Exception("You cannot put a ship diagonally, please enter " +
+                    "correct coordinates:");
+        }
+        else if (length != lengthFromCords) {
+            throw new Exception("Wrong length of the ship. Try again:");
+        }
+
+        for (int i = cords[0]; i <= cords[2]; i++) {
+            for (int j = cords[1]; j <= cords[3]; j++) {
+                if (battlefield.getBattlefield()[i - 1][j - 1] == ('O')) {
+                    throw new Exception("You cannot put one ship on top of another. Try again:");
+                }
+            }
+        }
+        for (int i = (cords[0] == 1 ? 1 : cords[0] - 1) ; i <= (cords[2] == 10 ? 10 : cords[2] + 1); i++) {
+            for (int j = (cords[1] == 1 ? 1 : cords[1] - 1); j <= (cords[3] == 10 ? 10 : cords[3] + 1); j++) {
+                if (battlefield.getBattlefield()[i - 1][j - 1] == ('O')) {
+                    throw new Exception("Error! You placed it too close to another one. Try again:");
+                }
+            }
+        }
+    }
+}
+
+class Battlefield {
+    private final char[][] battlefield = new char[10][10];
+    private final Ship[] ships;
+    private int shipNumber;
+
+
+    public Battlefield(Ship[] shipsInput) {
+        ships = shipsInput;
+        this.shipNumber = shipsInput.length;
+    }
+    public Ship getShip() { return this.ships[shipNumber - 1]; }
+
+    public char[][] getBattlefield() {
+        return battlefield;
+    }
+
+    public int getshipNumber() {
+        return this.shipNumber;
+    }
+
+    public void createBattlefield() {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                battlefield[i][j] = '~';
+            }
+        }
+    }
+
+    public void printBattlefield() {
+        System.out.println(" 1 2 3 4 5 6 7 8 9 10");
+        char[] firstCol = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        for (int i = 0; i < 10; i++) {
+            System.out.print(firstCol[i] + " ");
+            for (int j = 0; j < 10; j++) {
+                System.out.print(battlefield[i][j]);
+                if (j != 9) System.out.print(" ");
             }
             System.out.println();
         }
     }
-
-    private static String[][] boardInit() {
-        String[][] board = new String[11][11];
-        board[0][0] = " ";
-        String[] topRow = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
-        String[] firstCol = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
-        System.arraycopy(topRow, 0, board[0], 1, 10);
-        for (int i = 1; i < 11; i++) {
-            board[i][0] = firstCol[i - 1];
-        }
-        //
-        for (int i = 1; i < 11; i++) {
-            for (int j = 1; j < 11; j++) {
-                board[i][j] = "~";
+    public void placeShip() {
+        for (int i = getShip().getShipCords()[0] - 1; i <= getShip().getShipCords()[2] - 1; i++) {
+            for (int j = getShip().getShipCords()[1] - 1; j <= getShip().getShipCords()[3] - 1; j++) {
+                battlefield[i][j] = 'O';
             }
         }
-        return board;
+        this.shipNumber--;
     }
 
-    private static void placeShip(String[][] board, int[] shipCords) {
-        for (int i = shipCords[0]; i <= shipCords[2]; i++) {
-            for (int j = shipCords[1]; j <= shipCords[3]; j++) {
-                board[i][j] = "O";
-            }
-        }
+}
+
+class Ship {
+    private final String shipName;
+    private final int shipLength;
+    private int[] shipCords;
+
+    public Ship(String name, int length) {
+        shipName = name;
+        shipLength = length;
     }
 
+    public String getShipName() { return shipName; }
 
-    private static void p1PlaceShips(Scanner scanner, String[][] board) {
-        Pattern pattern = Pattern.compile("[A-J]\\d{1,2}\\s[A-J]\\d{1,2}");
-        String[] ships = {"Aircraft Carrier (5 cells)", "Battleship (4 cells)", "Submarine (3 cells)",
-                "Cruiser (3 cells)", "Destroyer (2 cells)"};
-        int[] shipsLengths = {5, 4, 3, 3, 2};
-        int[] shipCords = new int[4];
-        for (int i = 0; i < 5; i++) {
-            boolean inputPattern = false;
-            System.out.printf("Enter the coordinates of the %s: \n", ships[i]);
-            String coordinates;
-            Matcher m;
-            while (!inputPattern) {
-                coordinates = scanner.nextLine();
-                m = pattern.matcher(coordinates);
-                inputPattern = m.find();
-                if (inputPattern) {
-                    shipCords = convertCords(coordinates);
-                    boolean cords = checkIfValid(board, shipCords, shipsLengths[i]);
-                        if(!cords) {
-                            inputPattern = false;
-                        }
-                } else System.out.println("Please put coordinates in correct format:");
-            }
-            placeShip(board, shipCords);
-            printBoard(board);
+    public int getShipLength() { return shipLength; }
+
+    public int[] getShipCords() { return this.shipCords; }
+
+    public void setShipCords(String cords) {
+
+        String[] cordsPrep = cords.split(" ");
+        this.shipCords = new int[4];
+        this.shipCords[0] = (int) cordsPrep[0].charAt(0) - 64;
+        this.shipCords[1] = Integer.parseInt(cordsPrep[0].replaceAll("[A-Z]", ""));
+        this.shipCords[2] = (int) cordsPrep[1].charAt(0) - 64;
+        this.shipCords[3] = Integer.parseInt(cordsPrep[1].replaceAll("[A-Z]", ""));
+
+        if(this.shipCords[0] + this.shipCords[1] > this.shipCords[2] + this.shipCords[3]) {
+            this.shipCords = new int[] {this.shipCords[2], this.shipCords[3],
+                    this.shipCords[0], this.shipCords[1]};
         }
-        scanner.close();
-    }
-
-    private static int[] convertCords(String coordinates) {
-        // cords are strings where _ _ _ _ _ are: start x, start y, space, end x, end y;
-        int[] cords = new int[4];
-        String[] cordsPrep = coordinates.split(" ");
-        cords[0] = (int) cordsPrep[0].charAt(0) - 64;
-        cords[1] = Integer.parseInt(cordsPrep[0].replaceAll("[A-Z]", ""));
-        cords[2] = (int) cordsPrep[1].charAt(0) - 64;
-        cords[3] = Integer.parseInt(cordsPrep[1].replaceAll("[A-Z]", ""));
-
-        if(cords[0] + cords[1] > cords[2] + cords[3]) {
-            return new int[] {cords[2], cords[3], cords[0], cords[1]};
-        }
-
-        return cords;
-    }
-
-    private static boolean checkIfValid(String[][] board, int[] cords, int length) {
-        if (cords[1] > 10 || cords[3] > 10) {
-            System.out.println("Coordinates out of board area:");
-            return false;
-        }
-//     TODO
-//      [ ] - Check if the placement of the ship is valid
-//          [*] - check if correct ship is being placed
-//          [*] - check if cords are not diagonal
-//              if  horizontal then cords[0] == cords[2]
-//              if vertical then cords[1] == cords[3]
-//          [*] - They cannot be on top of each other
-//          [*] - There has to be at least one space in between ships
-
-        if (!(cords[0] == cords[2] && cords[1] != cords[3]
-        || cords[0] != cords[2] && cords[1] == cords[3])) {
-            System.out.println("You cannot put a ship diagonally, please enter " +
-                    "correct coordinates:");
-            return false;
-        };
-
-        if (cords[0] == cords[2]) if (length != Math.abs(cords[1] - cords[3]) + 1) {
-            System.out.println("Wrong length of the ship. Try again:");
-            return false;
-        };
-        if (cords[1] == cords[3]) if (length != Math.abs(cords[0] - cords[2]) + 1) {
-            System.out.println("Wrong length of the ship. Try again:");
-            return false;
-        };
-
-        for (int i = cords[0]; i <= cords[2]; i++) {
-            for (int j = cords[1]; j <= cords[3]; j++) {
-                if (board[i][j].matches("O")) {
-                    System.out.println("You cannot put one ship on top of another. Try again:");
-                    return false;
-                };
-            }
-        }
-// what is wrong with the code below?
-        for (int i = (cords[0] == 1 ? 1 : cords[0] - 1) ; i <= (cords[2] == 10 ? 10 : cords[2] + 1); i++) {
-            for (int j = (cords[1] == 1 ? 1 : cords[1] - 1); j <= (cords[3] == 10 ? 10 : cords[3] + 1); j++) {
-                if (board[i][j].matches("O")) {
-                    System.out.println("Error! You placed it too close to another one. Try again:");
-                    return false;
-                };
-            }
-        }
-
-
-        return true;
     }
 }
